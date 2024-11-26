4f8c0d5fd4c2b3ed990d8262c6b2ae607963b0f9
JBRS307 <kuba.rekas.307@gmail.com>
2024-11-26 16:12:18 +0100

    initial commit created spring project

---
+++ backend/src/test/java/pl/agh/to/lang/LangApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.agh.to.lang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
