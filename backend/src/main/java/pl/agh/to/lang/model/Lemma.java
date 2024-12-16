0cb89332468c548c3c9b6f549c0842885c4e8377
Eryk Hadała <eryk.hadala@divstack.pl>
2024-12-16 21:55:36 +0100

    fix: Lemma validations

--- backend/src/main/java/pl/agh/to/lang/model/Lemma.java
+++ backend/src/main/java/pl/agh/to/lang/model/Lemma.java
@@ -6,7 +6,6 @@ import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -17,7 +16,7 @@ public class Lemma {
     @NotBlank
     private String name;
 
-    @NotNull
+    @NotBlank
     private String translation;
 
     @Id
