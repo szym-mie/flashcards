76f6ad9a2888046763a5b5c393ffade01f248b9c
szym-mie <szym.mie@gmail.com>
2025-01-08 15:31:53 +0100

    Fix rather minor things (convert to records, cleanup imports)

--- backend/src/main/java/pl/agh/to/lang/model/Sentence.java
+++ backend/src/main/java/pl/agh/to/lang/model/Sentence.java
@@ -2,15 +2,13 @@ package pl.agh.to.lang.model;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Value;
 
 @Value
-@AllArgsConstructor
 public class Sentence {
     @NotBlank
-    private final String text;
+    String text;
 
     @NotNull
-    private final Language language;
+    Language language;
 }
