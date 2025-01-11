12ef243816b7b9275895f7d48b69216ebfe68854
szym-mie <szym.mie@gmail.com>
2025-01-11 13:03:42 +0100

    Refactor @Value annotated classes

--- backend/src/main/java/pl/agh/to/lang/dto/FlashcardsResponse.java
+++ backend/src/main/java/pl/agh/to/lang/dto/FlashcardsResponse.java
@@ -1,16 +1,14 @@
 package pl.agh.to.lang.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Value;
 import pl.agh.to.lang.model.Flashcard;
 import pl.agh.to.lang.model.Sentence;
 
 import java.util.List;
 
-@AllArgsConstructor
 @Value
 public class FlashcardsResponse {
-    private final Sentence sentence;
+    Sentence sentence;
 
-    private final List<Flashcard> flashcards;
+    List<Flashcard> flashcards;
 }
